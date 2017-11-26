0b61407d20e7155348613cfa06716b2cd0e8f1cc
Septiano Praditya <septianopraditya19@gmail.com>
2017-11-26 18:43:17 +0700

    DetailMatakuliah
    Function Controller detailInformasiMatakuliah

--- app/Http/Controllers/MatakuliahController.php
+++ app/Http/Controllers/MatakuliahController.php
@@ -24,27 +24,19 @@ class MatakuliahController extends Controller
         return view ('mahasiswa.informasiMatkul', ['matakuliahs' => $matakuliahs]);
     }
 
+    public function detailInformasiMatkul($kodeMK)
+    {
+        $kodeMatkul =DB::table('mata_kuliahs')->where('mata_kuliahs.kode_matakuliah', $kodeMK)->groupby('kode_matakuliah')->get();
+        $matakuliahs = DB::table('mata_kuliahs')->where('mata_kuliahs.kode_matakuliah', $kodeMK)->get();
+        $array = array($kodeMatkul, $matakuliahs);
+        return view ('detailInformasiMatakuliah', ['array' => $array]);
+    }
+
     public function jadwalMatkulMah()
     {
         $matakuliahs = DB::table('mata_kuliahs')->join('ruangans','mata_kuliahs.ruangan_id','=','ruangans.id')->join('jadwals','mata_kuliahs.jadwal_id','=','jadwals.id')->select('jadwals.hari as hari','jadwals.jam_mulai as jam_mulai','jadwals.jam_selesai as jam_selesai','mata_kuliahs.kode_matakuliah as kode_matakuliah','mata_kuliahs.nama as nama_matakuliah','kp as kp','kapasitas as kapasitas','ruangans.nama as nama_ruangan')->get();
         // $dosenajars = DB::table('mata_kuliahs')->join('dosens_ajar_mata_kuliahs','mata_kuliahs','=','')
         return view ('mahasiswa.jadwalMatkul', ['matakuliahs' => $matakuliahs]);
-    }
-
-    public function inputMatkulBaru()
-    {
-        $jadwals = DB::table('jadwals')->get();
-        $ruangans = DB::table('ruangans')->get();
-        $jurusans = DB::table('jurusans')->get();
-        $arrayData = array($jadwals, $ruangans, $jurusans);
-        return view('karyawan.InputMatkul',['arrayData'=>$arrayData]);
-    }
-
-    public function transkrip()
-    {
-        $id = Auth::user()->id;
-        DB::table('mahasiswas')->where('mahasiswas.user_id',$id)->get();
-        show($id);
     }
     /**
      * Show the form for creating a new resource.
@@ -53,8 +45,7 @@ class MatakuliahController extends Controller
      */
     public function create()
     {
-        
-          
+        //
     }
 
     /**
@@ -66,17 +57,7 @@ class MatakuliahController extends Controller
     public function store(Request $request)
     {
         //
-        $matakuliahs = new Matakuliah(
-            array(
-                'kode_matakuliah' => $request->get('kode'),
-                'nama' => $request->get('nama'),'sks'=>$request->get('sks'),'kp'=>$request->get('kp'),'jurusan_id'=>$request->get('jurusan'),'jadwal_id'=>$request->get('jadwal1'),'ruangan_id'=>$request->get('ruangan1')
-                )
-            );
-        $matakuliahs->save();
-        return redirect('/inputMatkulBaru')
-        ->with('status', 'Matkul dengan nama '
-            .' sudah berhasil disimpan');
-        
+    
     }
 
     /**
@@ -88,8 +69,7 @@ class MatakuliahController extends Controller
     public function show($id)
     {
         //
-        $matakuliahs = DB::table('mahasiswas_ambil_mata_kuliahs')->join('mata_kuliahs','mahasiswas_ambil_mata_kuliahs.mata_kuliah_id','=','mata_kuliahs.id')->where([
-            ['mahasiswas_ambil_mata_kuliahs.mahasiswa_id',$id],['mahasiswas_ambil_mata_kuliahs.status','Selesai']])->get();
+        $matakuliahs = DB::table('mahasiswas_ambil_mata_kuliahs')->join('mata_kuliahs','mahasiswas_ambil_mata_kuliahs.mata_kuliah_id','=','mata_kuliahs.id')->where('mahasiswas_ambil_mata_kuliahs.mahasiswa_id',1)->get();
         return view ('mahasiswa.transkrip', ['matakuliahs' => $matakuliahs]);
     }
 
